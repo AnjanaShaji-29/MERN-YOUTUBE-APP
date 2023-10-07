fb459a71785b2defd1700e2a213416dd6836f626
AnjanaShaji29 <AnjanaShaji29>
2023-10-07 19:47:49 +0530

    JWT cookie Authentication added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
 
@@ -12,7 +14,33 @@ export const signup = async (req, res, next) => {
         await newUser.save(); // Saving new user to DB
         res.status(200).send("User has been created"); // user created success message
 
+        
     } catch(err){
-       next(err);
+       next(err); // Error
+    }
+}
+
+
+export const signin = async (req, res, next) => {
+
+    try{
+        const user =  await User.findOne({name: req.body.name}) // Finding user by name from DB
+        if(!user)
+        return next(createError(404, "User not found!")); // User not found error message
+
+        const isCorrect = bcrypt.compare(req.body.password, user.password); // Comparing stored & readed password 
+        if(!isCorrect)
+        return next(createError(404, "Password doesn't match!")); // Password not match error message
+
+        const token = jwt.sign({id: User._id}, "process.env.JWT") // Creating token
+        const { password, ...others} = user._doc; // Prevent sending password & other info 
+        
+        res.cookie("access_token", token, { 
+            httpOnly: true // Make more secure by blocking third party apps
+        }).status(200).json(others); // Sending user details except password
+
+
+    } catch(err){
+       next(err); // Error
     }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,29 @@
 import express from "express";
-// import { test } from "../controllers/user.js";
+import { dislike, getUser, like, remove, subscribe, unsubscribe, update } from "../controllers/user.js";
+
 
 const router = express.Router();
+
+// update user
+router.put("/:id", update)
+
+// delete user
+router.delete("/:id", remove)
+
+// get a user
+router.get("/find/:id", getUser)
+
+//subscribe a user
+router.put("/sub/:id", subscribe)
+
+//Unsubscribe a user
+router.put("/unsub/:id", unsubscribe)
+
+// like a video
+router.put("/like/:videoId", like)
+
+// Dislike a video
+router.put("/unlike/:videoId", dislike)
+
 
 export default router;
