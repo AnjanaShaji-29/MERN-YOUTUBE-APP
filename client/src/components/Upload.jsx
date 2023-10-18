a928bb100565666a84aeb591d928291563802fd3
AnjanaShaji29 <AnjanaShaji29>
2023-10-18 19:28:58 +0530

    React Upload File & adding Url to MongoDB added

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { getStorage, ref, uploadBytesResumable,getDownloadURL, } from "firebase/storage";
+import app from '../firebase.js';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
 
 const Container = styled.div`
     width:100%;
@@ -66,23 +71,124 @@ const Label = styled.label`
   font-size: 14px;
 `;
 
-const Upload = ({setOpen}) => {
+const Upload = ({ setOpen }) => {
+
+  const [img, setImg] = useState(undefined);
+  const [video, setVideo] = useState(undefined);
+
+  const [imgPerc, setImgPerc] = useState(0); // For showing progress of image upload
+  const [videoPerc, setVideoPerc] = useState(0); // For showing progress of video upload
+
+  const [inputs, setInputs] = useState({}); // For title,desc,..
+  const [tags, setTags] = useState([]); // tags
+
+  const navigate = useNavigate(); // Change the location 
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value }; // Taking all previous inputs & changing tht required one
+    });
+  };
+
+  const handleTags = (e) => {
+    setTags(e.target.value.split(",")); //  Splitting the tags by ,
+  };
+
+  const uploadFile = (file, urlType) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name; // To avoid duplication of filename
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === "imgUrl" ? setImgPerc(Math.round(progress)) : setVideoPerc(Math.round(progress));
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {},
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setInputs((prev) => {
+            return { ...prev, [urlType]: downloadURL };
+          });
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    video && uploadFile(video , "videoUrl"); // If there is a video, call uploadFile
+  }, [video]); // Dependency
+
+  useEffect(() => {
+    img && uploadFile(img, "imgUrl"); // If there is an image, call uploadFile
+  }, [img]); // Dependency
+
+  const handleUpload = async (e)=>{
+    e.preventDefault(); // Prevent refreshing the page
+    const res = await axios.post("/videos", {...inputs, tags});  // Sending the video details to DB
+    setOpen(false); // Closing the popup
+    res.status===200 && navigate(`/video/${res.data._id}`); // If success, navigate to video page
+  }
+
   return (
     <Container>
-        <Wrapper>
-            <Close onClick={()=> setOpen(false)}> X </Close>
-            <Title> Upload a New Video </Title>
-            <Label> Video: </Label>
-            <Input type="file" accept='video/*'/>
-            <Input type="text" placeholder='Title' />
-            <Desc placeholder='Description' rows={8} />
-            <Input type="text" placeholder='Separate the tags with commas.' />
-            <Label> Image: </Label>
-            <Input type="file" accept='image/*'/>
-            <Button> Upload </Button>
-        </Wrapper>
+      <Wrapper>
+        <Close onClick={() => setOpen(false)}>X</Close>
+        <Title>Upload a New Video</Title>
+        <Label>Video:</Label>
+        {videoPerc > 0 ? (
+          "Uploading:" + videoPerc
+        ) : (
+          <Input
+            type="file"
+            accept="video/*"
+            onChange={(e) => setVideo(e.target.files[0])}
+          />
+        )}
+        <Input
+          type="text"
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+        />
+        <Desc
+          placeholder="Description"
+          name="desc"
+          rows={8}
+          onChange={handleChange}
+        />
+        <Input
+          type="text"
+          placeholder="Separate the tags with commas."
+          onChance={handleTags}
+        />
+        <Label>Image:</Label>
+        {imgPerc > 0 ? (
+          "Uploading:" + imgPerc + "%"
+        ) : (
+          <Input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setImg(e.target.files[0])}
+          />
+        )}
+        <Button onClick={handleUpload}>Upload</Button>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
 export default Upload;
